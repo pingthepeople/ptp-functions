444812092e2f30198084acba12109cf8b19c8b20
John Hoerr <jhoerr@iu.edu>
2017-02-20 15:00:37 -0500

    Add SMS notification to integration test

--- test/integration.fsx
+++ test/integration.fsx
@@ -1,17 +1,26 @@
 #r "../packages/WindowsAzure.ServiceBus/lib/net45-full/Microsoft.ServiceBus.dll"
+#r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 #load "../shared/model.fs"
 
 open System
 open System.Runtime.Serialization
 open Microsoft.ServiceBus
 open Microsoft.ServiceBus.Messaging
+open Newtonsoft.Json
 open IgaTracker.Model
-let enqueueEmailNotification text =
+
+let enqueueNotification message = 
     let queueName = "notification"
     let connectionString = System.Environment.GetEnvironmentVariable("ServiceBus.ConnectionString")
     let queueClient = QueueClient.CreateFromConnectionString(connectionString, queueName)
-    let message = {MessageType=MessageType.Email; Recipient="jhoerr@gmail.com"; Subject=text; Body=text}
-    let brokeredMessage = new BrokeredMessage(message)
+    let json = JsonConvert.SerializeObject(message)
+    let brokeredMessage = new BrokeredMessage(json)
     queueClient.Send(brokeredMessage)
 
-enqueueEmailNotification (sprintf "test message %s" (DateTime.Now.ToString()))
+let text = (sprintf "test message %s" (DateTime.Now.ToString()))
+
+// Test sendNotification email handling by sending email message to 'notification' queue
+enqueueNotification {MessageType=MessageType.Email; Recipient="CHANGEME"; Subject=text; Body=text}
+
+// Test sendNotification SMS handling by sending SMS message to 'notification' queue
+enqueueNotification {MessageType=MessageType.SMS; Recipient="+1CHANGEME"; Subject=text; Body=text}
