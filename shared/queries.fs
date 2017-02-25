691916f9e172e1657e5d14388b11d53530801528
John Hoerr <jhoerr@iu.edu>
2017-02-24 20:20:19 -0500

    Add query for generating tabular leg progress reports

--- shared/queries.fs
+++ shared/queries.fs
@@ -64,3 +64,44 @@ WHERE NOT EXISTS(
 		tbl.BillId = cte.BillId
 		AND tbl.CommitteeId = cte.CommitteeId
 		AND tbl.Assigned = cte.Assigned)"""
+    
+    let GenerateSpreadSheetReport = """SELECT
+	b.Name as 'Topics'
+	, b.Title
+	, b.Description
+	, b.Authors
+	, (Select oc.Name from Committee oc Join BillCommittee obc on oc.Chamber = b.Chamber and b.Id = obc.BillID and oc.Id = obc.CommitteeId) as 'Origin Committee'
+	, (Select cc.Name from Committee cc Join BillCommittee cbc on cc.Chamber <> b.Chamber and b.Id = cbc.BillID and cc.Id = cbc.CommitteeId) as 'Crossover Committee'
+	, STUFF( (SELECT '; ' + s.Name
+                             FROM [Subject] s
+							 Join [BillSubject] bs on s.Id = bs.SubjectId
+							 WHERE bs.BillId = b.Id
+							 --Order by s.Id
+                             FOR XML PATH('')), 
+                            1, 1, '') as 'Topics'
+	, ISNULL(aocr.Date,socr.Date) as 'Origin Committee Reading'
+	, aocr.Description as 'Vote'
+	, ISNULL(ao2r.Date,so2r.Date) 'Second Reading'
+	, ao2r.Description as 'Vote'
+	, ISNULL(ao3r.Date,so3r.Date) 'Third Reading'
+	, ao3r.Description as 'Vote'
+	, ISNULL(accr.Date,sccr.Date) 'Crossover Committee Reading'
+	, accr.Description 'Vote'
+	, ISNULL(ac2r.Date,sc2r.Date) 'Second Reading'
+	, ac2r.Description 'Vote'
+	, ISNULL(ac3r.Date,sc3r.Date) 'Third Reading'
+	, ac3r.Description 'Vote'
+from Bill b
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 1 and a.Chamber = b.Chamber) aocr
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 2 and a.Chamber = b.Chamber) ao2r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 3 and a.Chamber = b.Chamber) ao3r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 1 and a.Chamber <> b.Chamber) accr
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 2 and a.Chamber <> b.Chamber) ac2r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date], Description from [Action] a where a.BillId = b.Id and ActionType = 3 and a.Chamber <> b.Chamber) ac3r
+--
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 1 and sa.Chamber = b.Chamber) socr
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 2 and sa.Chamber = b.Chamber) so2r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 3 and sa.Chamber = b.Chamber) so3r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 1 and sa.Chamber <> b.Chamber) sccr
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 2 and sa.Chamber <> b.Chamber) sc2r
+outer apply (Select Top 1 CAST([Date] AS DATE) [Date] from [ScheduledAction] sa where sa.BillId = b.Id and ActionType = 3 and sa.Chamber <> b.Chamber) sc3r"""
