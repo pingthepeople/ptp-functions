2d9ad19250c047eab612eafb37e1920f334bd2f4
John Hoerr <jhoerr@iu.edu>
2017-03-09 15:13:35 -0500

    Refactor back for convenience

--- updateActions/run.fsx
+++ updateActions/run.fsx
@@ -1,7 +1,12 @@
 
+#r "System.Data"
+#r "../packages/Dapper/lib/net45/Dapper.dll"
 #r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+#r "../packages/FSharp.Data/lib/portable-net45+sl50+netcore45/FSharp.Data.dll"
 
-#load "module.fsx"
+#load "../shared/model.fs"
+#load "../shared/queries.fs"
+#load "../shared/db.fsx"
 #load "../shared/http.fsx"
 
 open System
@@ -15,8 +20,38 @@ open IgaTracker.Model
 open IgaTracker.Http
 open IgaTracker.Db
 open IgaTracker.Queries
-open IgaTracker.UpdateActions
 open Newtonsoft.Json
+
+let toActionModel (action,bill:Bill) = {
+    Action.Id = 0;
+    Date = action?date.AsDateTime();
+    Link = action?link.AsString();
+    ActionType = action?description.AsString() |> Action.ParseType;
+    Description = action?description.AsString() |> Action.ParseDescription;
+    Chamber = System.Enum.Parse(typeof<Chamber>, action?chamber?name.AsString()) :?> Chamber
+    BillId = bill.Id;
+}
+
+let addToDatabase date (cn:SqlConnection) allActions =
+
+    let bills = cn |> dapperQuery<Bill> SelectBillIdsAndNames
+    let links = cn |> dapperParametrizedQuery<string> SelectActionLinksOccuringAfterDate {DateSelectArgs.Date=date}
+
+    let addActionToDbAndGetId (action:Action) = cn |> dapperParametrizedQuery<int> InsertAction action |> Seq.head
+    let fetchActionsRequiringAlert insertedIds = cn |> dapperMapParametrizedQuery<Action> SelectActionsRequiringNotification (Map ["Ids", insertedIds :> obj])
+
+    let toKnownBills action = bills |> Seq.exists (fun bill -> bill.Name = action?billName?billName.AsString())
+    let toUnrecordedActions action = links |> Seq.exists (fun link -> link = action?link.AsString()) |> not
+    let toKnownActionTypes (action:Action) = action.ActionType <> ActionType.Unknown
+    let actionAndBill action = (action, bills |> Seq.find (fun bill -> bill.Name = action?billName?billName.AsString()))
+
+    allActions
+        |> List.filter toKnownBills
+        |> List.filter toUnrecordedActions
+        |> List.map (actionAndBill >> toActionModel)
+        |> List.filter toKnownActionTypes
+        |> List.map addActionToDbAndGetId
+        |> fetchActionsRequiringAlert 
 
 // Azure Function entry point
 
