42bcc327d77cb4945659ff408a2fde1291cad138
John Hoerr <jhoerr@iu.edu>
2017-08-30 20:13:20 -0400

    Port GenerateBillReports function

---
+++ src/Functions/GenerateBillReport/GenerateBillReport.fs
@@ -0,0 +1,35 @@
+﻿module GenerateBillReport
+
+open Chessie.ErrorHandling
+open Microsoft.Azure.WebJobs.Host
+open Newtonsoft.Json
+open Ptp.Core
+open Ptp.Model
+open Ptp.Http
+open Ptp.Database
+open Ptp.Queries
+open System
+open System.Net
+open System.Net.Http
+open System.Data.SqlClient
+
+[<CLIMutable>]
+type Body  = { Id : int }
+
+let generateReport body = 
+    try
+        new SqlConnection(sqlConStr()) 
+        |> dapperMapParametrizedQuery<BillStatus> FetchBillStatusForUser (Map["Id", body.Id :> obj]) 
+        |> JsonConvert.SerializeObject
+        |> ok
+    with
+    | ex -> fail (HttpStatusCode.InternalServerError, ("Failed to generate report: " + ex.Message))
+
+let processRequest = 
+    validateBody<Body> "A user id is expected in the form '{ Id: INT }'"
+    >> bind generateReport
+
+let Run(req: HttpRequestMessage, log: TraceWriter) =
+    req
+    |> processRequest
+    |> constructHttpResponse
