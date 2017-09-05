19f7dceaae0f830f766838c549dfd7dc4d719625
John Hoerr <jhoerr@iu.edu>
2017-09-05 10:28:36 -0400

    Add a manually-triggered function for bulk-loading bills at the start of the session.

---
+++ src/Functions/UpdateCanonicalDataManual/UpdateCanonicalDataManual.fs
@@ -0,0 +1,21 @@
+﻿module UpdateCanonicalDataManual
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open Ptp.Core
+open Ptp.Database
+open Ptp.UpdateCanonicalData.Bills
+open Ptp.UpdateCanonicalData.Subjects
+open System.Data.SqlClient
+
+let updateCanonicalData (log:TraceWriter) =
+    use cn = new SqlConnection(sqlConStr())
+    let sessionYear = cn |> currentSessionYear
+    let sessionId = cn |> currentSessionId
+
+    updateSubjects    log cn sessionId sessionYear |> ignore
+    updateBills       log cn sessionId sessionYear |> ignore
+
+let Run(input: string, log: TraceWriter) =
+     updateCanonicalData 
+     |> tryRun "UpdateCanonicalData" log 
