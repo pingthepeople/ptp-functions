24f354214a81b76683c249e72b8ad5f497305b28
John Hoerr <jhoerr@gmail.com>
2018-12-22 17:25:56 -0500

    Make bindings more explicit; add heartbeat function

--- src/Functions/Functions.fs
+++ src/Functions/Functions.fs
@@ -17,9 +17,6 @@ open Serilog
 open Serilog.Core
 open Serilog.Context
     
-[<Literal>]
-let sbConnection = "ServiceBus.ConnectionString"
-
 let logger =
     let appInsightsKey = System.Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY")
     Serilog.Debugging.SelfLog.Enable(Console.Out);
@@ -38,16 +35,23 @@ let enqueue (collector:ICollector<string>) workflow =
     |> JsonConvert.SerializeObject
     |> collector.Add
 
+[<FunctionName("Heartbeat")>]
+let Heartbeat 
+    ([<TimerTrigger("0 */1 * * * *")>] timer, 
+        log: Microsoft.Extensions.Logging.ILogger) =
+        log.LogInformation "Heartbeat (ILogger)"
+        logger.Information "Heartbeat (Serilog)"
+
 [<FunctionName("UpdateLegislators")>]
 let UpdateLegislators 
     ([<TimerTrigger("0 0 6 * * 1-5")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
         Workflow.UpdateLegislators |> enqueue nextCommand
 
 [<FunctionName("UpdateCommittees")>]
 let UpdateCommittees 
     ([<TimerTrigger("0 10 6 * * 1-5")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
     Console.Out.WriteLine("Enqueuing UpdateCommittees...")
     Workflow.UpdateCommittees |> enqueue nextCommand
     Console.Out.WriteLine("Enqueued UpdateCommittees.")
@@ -55,35 +59,35 @@ let UpdateCommittees
 [<FunctionName("UpdateSubjectsAndBills")>]
 let UpdateSubjectsAndBills
     ([<TimerTrigger("0 20 6 * * 1-5")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
     Workflow.UpdateSubjects |> enqueue nextCommand
 
 [<FunctionName("UpdateCalendarsAndActions")>]
 let UpdateCalendarsAndActions
     ([<TimerTrigger("0 */10 8-20 * * 1-5")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
     Workflow.UpdateCalendars |> enqueue nextCommand
     Workflow.UpdateActions |> enqueue nextCommand
 
 [<FunctionName("UpdateDeadBills")>]
 let UpdateDeadBills
     ([<TimerTrigger("0 0 9 * * 1-6")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
     Workflow.UpdateDeadBills |> enqueue nextCommand
 
 [<DisableAttribute("DISABLE_DAILY_ROUNDUP")>]
 [<FunctionName("DailyRoundup")>]
 let DailyRoundup
     ([<TimerTrigger("0 30 19 * * 1-5")>] timer,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>) =
     Workflow.DailyRoundup |> enqueue nextCommand
 
 [<FunctionName("Workflow")>]
 let Workflow
-    ([<ServiceBusTrigger("command", Connection=sbConnection)>] command,
+    ([<ServiceBusTrigger("command", Connection="ServiceBus.ConnectionString")>] command,
         log: Microsoft.Extensions.Logging.ILogger,
-        [<ServiceBus("command", Connection=sbConnection)>] nextCommand: ICollector<string>,
-        [<ServiceBus("notification", Connection=sbConnection)>] notifications: ICollector<string>) =
+        [<ServiceBus("command", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] nextCommand: ICollector<string>,
+        [<ServiceBus("notification", EntityType.Queue, Connection="ServiceBus.ConnectionString")>] notifications: ICollector<string>) =
     Ptp.Workflow.Orchestrator.Execute log command nextCommand notifications
 
 
@@ -91,7 +95,7 @@ let Workflow
 
 [<FunctionName("Notifications")>]
 let Notification
-    ([<ServiceBusTrigger("notification", Connection=sbConnection)>] notification, 
+    ([<ServiceBusTrigger("notification", Connection="ServiceBus.ConnectionString")>] notification, 
         log: Microsoft.Extensions.Logging.ILogger) =
     Ptp.Messaging.Notification.Execute log notification
 
